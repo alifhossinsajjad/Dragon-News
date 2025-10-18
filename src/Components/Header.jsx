b114a99d01babcf71ae8899c70f5cbefdfeb202a
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-10-18 22:16:38 +0600

    hader section done with set date week and year

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import logo from '../assets/logo.png'
+import { format } from 'date-fns';
 const Header = () => {
     return (
-        <div>
-            
+        <div className='grid justify-center space-y-3'>
+            <img src={logo} alt="" />
+            <p className='text-accent'>Journalism Without Fear or Favour</p>
+            <p className='font-semibold text-accent'>{format(new Date() ,"EEEE, MMMM MM , yyyy")}</p>
         </div>
     );
 };
